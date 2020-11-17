dbe0af7a7d67d0175459da0b2f53c96b0b2371e6
Cupcat123 <71680019+Cupcat123@users.noreply.github.com>
2020-11-17 10:38:59 +0800

    ASIA_AR_Midterm_107051063

---
+++ AR_Midterm_107051063/Assets/script/Buttom.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class Buttom : MonoBehaviour
+{
+
+    [Header("女")]
+    public Animator female;
+
+    [Header("男")]
+    public Animator male;
+
+    public void Attack()
+    {
+        female.SetTrigger("攻擊");
+        male.SetTrigger("攻擊");
+    }
+
+    public void Run()
+    {
+        female.SetTrigger("跑步");
+        male.SetTrigger("跑步");
+    }
+}
